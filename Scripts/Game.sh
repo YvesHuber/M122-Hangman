d3f15b42d3d09430dccba3f90db66c9324ce3d67
YvesHuber <cpt.rino@gmail.com>
2023-06-12 14:09:56 +0200

    game working

--- Scripts/Game.sh
+++ Scripts/Game.sh
@@ -1,6 +1,8 @@
-#Dieser Code ist eigentum der Bundesrepublik deutschland
+#!/bin/bash
 
+#Dieser Code ist eigentum der Bundesrepublik deutschland
 source ./GameLoop.sh
+
 echo "Welcome to our Hangman game"
 echo "Enter the user name: "  
 read userName
@@ -9,8 +11,4 @@ export M122UserName=$userName
 #defined
 echo "Welcome $M122UserName to our Hangman game"
 
-
 playGame
-
-
-
